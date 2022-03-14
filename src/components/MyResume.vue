a0a158b7e31dffec61361d5a3be9149ab3b01830
leonnn124 <s0976144676s@gmail.com>
2022-03-14 13:04:23 +0800

    0

---
+++ src/components/MyResume.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <nav class="navbar">
+      <button>
+        <i class="flex fas fa-bars"></i>
+      </button>
+    </nav>
+  </div>
+</template>
+
+<script>
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="postcss" scoped>
+
+</style>
